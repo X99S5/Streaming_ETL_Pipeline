d8b6375bb9bf237bbe8b80466b97ce240fd0b958
X99S5 <X99S5RPT@gmail.com>
2023-10-23 14:21:00 +0100

    Set up multiple kafka broker system / Started to integrate airflow / Extended python packages in airflow Dag scripts

--- Airflow_Docker/dags/Dag1.py
+++ Airflow_Docker/dags/Dag1.py
@@ -1,5 +1,34 @@
 from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime,timedelta
+# docker compose up -d --no-deps --build airflow-webserver airflow-scheduler airflow-triggerer (to rebuild image after adding additional packages)
+from kafka import KafkaProducer
+from json import dumps
+from time import sleep
+
+def topic_message():
+    topic_name = 'topic_a'
+    producer = KafkaProducer( bootstrap_servers=['localhost:9092'], value_serializer=lambda x:dumps(x).encode('utf-8'))
+    
+
+default_args = {
+    'owner' : 'me9',
+    'retries' : 5,
+    'retry_delay': timedelta(minutes = 2)
+
+}
 
 with DAG(
+    dag_id = '1',
+    default_args = default_args,
+    description = 'DAG #1',
+    start_date = datetime(2023,10,23, 11, 13, 0),
+    schedule_interval = '@daily'
     
 ) as dag:
+    task1 = PythonOperator(
+        task_id = 'topic_message',
+        python_callable=topic_message
+    )
+
+    task1
